e82ad01ff3d93d5ec466c703feb84eb9fa5b0b35
utkarshshu1221 <utkarshrajshubham@gmail.com>
2025-11-24 01:17:04 +0530

    update

--- Vada_Pav/street_food/src/lib/sanity.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@sanity/client";
-
-export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: "production",
-  apiVersion: "2023-01-01",
-  useCdn: true,
-});
